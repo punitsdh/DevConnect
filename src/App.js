90f7e43fbbbccbbc3dfc681475a7e29a5c77cf51
punitsdh <punitsdh@gmail.com>
2025-04-05 12:05:57 +0530

    testing get, post, delete

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+    res.send("Got user details!");
+})
 
+
+app.post("/user", (req, res) => {
+    res.send("Updated user details!");
+})
+
+app.delete("/user", (req, res) => {
+    res.send("Deleted user details!");
+})
 
 app.use("/test", (req, res) => {
     res.send("test from server");
 });
 
-app.use("/hello", (req, res) => {
-    res.send("hellow!  from server");
-});
-
-app.use("/",(req, res) => {
-    res.send("Hello from server");
-});
 
 app.listen(7777, () =>{
     console.log("Server running on port 7777");
