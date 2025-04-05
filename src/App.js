42dccff4a47379668afff8f7778c4bcf1f042195
punitsdh <punitsdh@gmail.com>
2025-04-06 01:33:05 +0530

    init-database

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
+
+app.use(express.json());
+
+
+app.post("/signup", async (req, res) =>{
+    const user = new User({
+        firstName: "Punit",
+        lastName: "Dhande",
+        emailId: "punit@dhande.com",
+        password: "punit@123",
+    });
+
+    //try{
+        await user.save();
+        res.send("User Added Successfully!");
+    //}
+    //catch(err){
+        //res.status(400).send("Error saving the user:" + err.message);
+    //}
+});
 
 
 connectDB().then(() => {
--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://punitsdh:3Y2hLdsSMvBUeKKu@devconnect.ur9lkm5.mongodb.net/");
+    await mongoose.connect("mongodb+srv://punitsdh:3Y2hLdsSMvBUeKKu@devconnect.ur9lkm5.mongodb.net/devConnect");
 };
 
 module.exports = connectDB;
